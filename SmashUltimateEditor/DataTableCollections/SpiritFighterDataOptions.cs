f69fd4fdceb621f85446d7d7a94c46caf26c27e5
Angelo Gonzalez <angelo.m.gonzalez@gmail.com>
2020-09-09 20:49:17 -0500

    Implement SpiritFighter options.

---
+++ SmashUltimateEditor/DataTableCollections/SpiritFighterDataOptions.cs
@@ -0,0 +1,32 @@
+﻿using SmashUltimateEditor.DataTables.ui_fighter_spirit_aw_db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmashUltimateEditor.DataTableCollections
+{
+    public class SpiritFighterDataOptions
+    {
+        public List<SpiritFighter> _dataList;
+        public List<IDataTbl> dataList { get { return _dataList.OfType<IDataTbl>().ToList(); } }
+
+        public List<string> chara_id
+        {
+            get { return _dataList.Select(x => x.chara_id).Distinct().OrderBy(x => x).ToList(); }
+        }
+        public List<string> ui_spirit_id
+        {
+            get { return _dataList.Select(x => x.ui_spirit_id).Distinct().OrderBy(x => x).ToList(); }
+        }
+        public bool IsUnlockableFighter(string ui_spirit_id)
+        {
+            string fighter = _dataList.Where(x => x.ui_spirit_id == ui_spirit_id).Select(x => x.chara_id).FirstOrDefault();  // Get the fighter for the spirit.
+            
+            return fighter == default(string) ?     // If it didn't match, not unlockable.  
+                false 
+                : 
+                _dataList.Where(x => x.chara_id == fighter).First().ui_spirit_id == ui_spirit_id;   // If it matches, its a spirit linked to an unlockable.  Only first instance will be actually unlockable fighter.
+        }
+    }
+}
--- SmashUltimateEditor/DataTables/ui_fighter_spirit_aw_db/SpiritFighter.cs
+++ SmashUltimateEditor/DataTables/ui_fighter_spirit_aw_db/SpiritFighter.cs
@@ -11,5 +11,17 @@ namespace SmashUltimateEditor.DataTables.ui_fighter_spirit_aw_db
         // Use first field if XML_NAME is generic. 
         internal static string XML_FIRST_FIELD = "ui_spirit_id";
 
+
+
+        [Order]
+        public string ui_spirit_id { get; set; }
+        [Order]
+        public string chara_id { get; set; }
+        [Order]
+        public byte chara_color { get; set; }
+        [Order]
+        public bool _0x0fe61a19ad { get; set; }
+        [Order]
+        public string _0x15e7618b71 { get; set; }
     }
 }

5124f130202ac5a0266b78e2e090ded0843b7c2e
Alo81 <angelo.m.gonzalez@gmail.com>
2021-03-04 22:05:32 -0600

    Dictionary builder logic.

--- SmashUltimateEditor/Helpers/StatsHelper.cs
+++ SmashUltimateEditor/Helpers/StatsHelper.cs
@@ -45,13 +45,13 @@ namespace SmashUltimateEditor.Helpers
             }
         }
 
-        public static void WriteHashSetToFile(HashSet<string> dict, string fileName)
+        public static void WriteHashSetToFile(IEnumerable<string> dict, string fileName)
         {
             using StreamWriter writer = new StreamWriter(fileName);
 
             foreach (var word in dict.OrderBy(x => x))
             {
-                writer.WriteLine(word);
+                writer.WriteLine($"_{word}");
             }
 
             writer.Close();
@@ -78,6 +78,82 @@ namespace SmashUltimateEditor.Helpers
             }
 
             WriteHashSetToFile(dict, outFileName);
+        }
+
+        public static void CreateDictionaryFromFiles(string inFolderName, string outFileName, string fileTypeToSearch = "out.prc", string delim = "_")
+        {
+            var dict = new HashSet<string>();
+
+            GetDictionaryFromFiles(inFolderName, fileTypeToSearch, ref dict);
+
+            SplitDictionaryOnDelim(ref dict, delim);
+
+            WriteHashSetToFile(dict, outFileName);
+        }
+
+        public static void CreateDictionaryFromFilesByLength(string inFolderName, string outFileName, string fileTypeToSearch = "out.prc", string delim = "_")
+        {
+            var dict = new HashSet<string>();
+
+            GetDictionaryFromFiles(inFolderName, fileTypeToSearch, ref dict);
+
+            SplitDictionaryOnDelim(ref dict, delim);
+            for(int i = 0; i < dict.Max(x => x.Length); i++)
+            {
+                WriteHashSetToFile(dict.Where(x => x.Length == (i)), $"{outFileName}_U_{(i+1)}");
+            }
+        }
+
+        public static void SplitDictionaryOnDelim(ref HashSet<string> dict, string delim)
+        {
+            var subDict = new HashSet<string>();
+            foreach(var word in dict)
+            {
+                var subWords = word.Split(delim);
+
+                foreach (var subWord in subWords.Where(x => x.Length > 0 && !Char.IsDigit(x[0])))
+                {
+                    subDict.Add(subWord);
+                }
+            }
+
+            foreach(var word in subDict)
+            {
+                dict.Add(word);
+            }
+        }
+
+        public static void GetDictionaryFromFiles(string inFolderName, string fileType, ref HashSet<string> dict)
+        {
+            var delim = '_';
+            var dir = new DirectoryInfo(inFolderName);
+
+            foreach (var folder in dir.EnumerateDirectories())
+            {
+                GetDictionaryFromFiles(folder.FullName, fileType, ref dict);
+            }
+
+            foreach(var subFile in dir.EnumerateFiles().Where(x => x.Name.EndsWith(fileType)))
+            {
+                var stream = XmlHelper.GetStreamFromFile(subFile.FullName);
+                var reader = XmlHelper.GetXmlReaderFromStream(stream);
+
+                while (!reader.EOF)
+                {
+                    reader.Read();
+                    var hash = reader?.GetAttribute("hash");
+                    if(hash != null)
+                    {
+                        dict.Add(hash);
+                        reader.Read();
+                        dict.Add(reader?.Value);
+                    }
+                }
+            }
+
+
+
+
         }
 
 
